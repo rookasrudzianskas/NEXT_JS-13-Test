661171095ee687dc124d8308a2739b15ffa7491c
Rokas <rookas.rudzenskas@gmail.com>
2022-11-06 11:09:24 +0200

    Add delay to todos list
    This is to simulate a slow network connection.

--- app/(user)/todos/TodosList.tsx
+++ app/(user)/todos/TodosList.tsx
@@ -3,6 +3,9 @@ import {Todo} from "../../../typings";
 import Link from "next/link";
 
 const fetchTodos = async () => {
+    const timeout = Math.floor((Math.random() * 5 + 1) * 1000);
+    await new Promise((resolve) => setTimeout(resolve, timeout));
+
     const res = await fetch('https://jsonplaceholder.typicode.com/todos');
     const todos: Todo[] = await res.json();
     return todos;
--- app/(user)/todos/page.tsx
+++ app/(user)/todos/page.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Suspense } from 'react';
 import TodosList from "./TodosList";
 
 const Todos = ({}) => {
     return (
         <div>
-            <h1>This is where todos will be listed</h1>
+            <Suspense fallback={<p>Loading todos</p>}>
+                <h1 className="text-blue-500">Loading todos</h1>
+                <div className="flex space-x-2">
+                    {/* @ts-ignore */}
+                    <TodosList />
+                </div>
+            </Suspense>
+
+            <Suspense fallback={<p>Loading shopping trolley</p>}>
+                <h1 className="text-red-500">Loading shopping trolley</h1>
+                <div className="flex space-x-2">
+                    {/* @ts-ignore */}
+                    <TodosList />
+                </div>
+            </Suspense>
         </div>
     );
 };
