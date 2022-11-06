4a75aaf36684a899d747d68e651dc981ac998177
Rokas <rookas.rudzenskas@gmail.com>
2022-11-06 10:57:57 +0200

    Add search page head
    This commit adds the head component for the search page.
    
    The head component is responsible for adding the title to the page.

---
+++ app/search/head.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Head = ({}) => {
+    return (
+        <>
+            <title>Search Page</title>
+        </>
+    );
+};
+
+export default Head;
+// by Rokas with ❤️
