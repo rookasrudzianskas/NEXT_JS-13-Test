33915abde620744f7e0eaac22fd1686de5b7ccfb
Rokas <rookas.rudzenskas@gmail.com>
2022-11-06 10:52:23 +0200

    feat: add search results
    - add search results
    - add loading indicator

---
+++ app/search/[searchTerm]/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = ({}) => {
+    return (
+        <div>
+            Loading ... please wait
+        </div>
+    );
+};
+
+export default Loading;
+// by Rokas with ❤️
--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -20,7 +20,7 @@ type SearchResults = {
 
 const Search = async (searchTerm: string) => {
     const response = await fetch(`https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`);
-    const data = await response.json();
+    const data: SearchResults = await response.json();
     return data;
 }
 
@@ -28,7 +28,16 @@ const SearchResults = async ({ params: { searchTerm } }: PageProps) => {
     const searchResults = await Search(searchTerm)
     return (
         <div>
+            <p className="text-gray-500 text-sm">You searched for: {searchTerm}</p>
 
+            <ol className="space-y-5 p-5">
+                {searchResults?.organic_results?.map((result) => (
+                    <li key={result.position} className="list-decimal">
+                        <p className="font-bold">{result.title}</p>
+                        <p>{result.snippet}</p>
+                    </li>
+                ))}
+            </ol>
         </div>
     );
 };
